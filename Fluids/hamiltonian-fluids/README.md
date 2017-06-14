7ea2b7431bbbd461987239d7c8305f76ef77edda
Aidan Macdonald <amacdonald@knupath.com>
2017-06-14 13:41:22 -0700

    Wrote particle simulation

---
+++ Fluids/hamiltonian-fluids/README.md
@@ -0,0 +1,9 @@
+# Simulation of Hamiltonian Fluids
+
+We run a many particle simulation in a box using the 
+free particle Lagrangian and then compare the results to
+a simulation of the Fluid Lagrangian computed via the 
+formalism that I developed for translating
+[Many Particle Systems to Fluids](https://physics.stackexchange.com/questions/335603/least-action-with-local-equilibrium)
+
+For visual simplicity, I just simulate in 2D.
---
+++ Fluids/hamiltonian-fluids/particle.py
@@ -0,0 +1,34 @@
+"""
+2D Many Particle Simulation
+
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation, FFMpegWriter
+
+N_PARTICLES = int(1e6)
+
+upper_bnds =  np.array([[5, 5]])
+lower_bnds = -np.array([[5, 5]])
+
+X = np.random.rand(N_PARTICLES, 2) - 0.5
+P = np.random.randn(N_PARTICLES, 2)
+dt = 1e-3
+m = 1.0
+
+fig, ax = plt.subplots()
+ax.set_xlim(1.2*lower_bnds[0, 0], 1.2*upper_bnds[0, 0])
+ax.set_ylim(1.2*lower_bnds[0, 1], 1.2*upper_bnds[0, 0])
+
+scat = ax.scatter(X[:, 0], X[:, 1], s=np.pi, c='r', alpha=0.1)
+
+def update(frame_number):
+    global X, P
+    X += dt*P/m
+    P += -2*P*(1.0*(X < lower_bnds) + 1.0*(X > upper_bnds))
+    scat.set_offsets(X)
+
+writer = FFMpegWriter(fps=60, bitrate=5000)
+animation = FuncAnimation(fig, update, interval=10)
+animation.save('particles.mp4', writer=writer, dpi=600)
+#plt.show()
